abbe39eddfd0c7aba42beea16ac3c4ab57f558ef
TrisT <30376152+RealTrisT@users.noreply.github.com>
2017-11-04 07:04:26 +0000

    👍made it universal
    it will now parse any file into a dictionary of tags, each tag contains an array, elements of the array contain proprieties of each instance of that tag, example:
    <tag propriety1='value1' propriety2='value2'>
    <tag propriety1='value3' propriety2='value4'>
    <tag2 propriety3='value1' propriety4='value20'>
    
    Will result in:
    {
       'tag':[
          {'propriety1':'value1', propriety2':'value2'}, 
          {'propriety1':'value3', propriety2':'value4'}
       ]
       'tag2':[
          {'propriety3':'value1', propriety4':'value20'}
       ]
    }

--- config_parser.py
+++ config_parser.py
@@ -1,58 +1,87 @@
-def getPropriety(text, tag): 
-    if text.find(' '+tag+'=\'') != -1:
-        begin = text.find(' '+tag+'=\'')+3+len(tag)
-        if begin == -1:
-            return False
+def getPropriety(text, tag):
+    if text.find(' '+tag+'=\'') != -1: #if Propriety exists
+        begin = text.find(' '+tag+'=\'')+3+len(tag) #set begin to the beginning of the Propriety's value
         tempbegin = begin
         end = 0
-        while text.find('\'', tempbegin) != -1:
-            tempbegin = text.find('\'', tempbegin)+1
-            if text[tempbegin-1-1:tempbegin-1] == '\\':
+        while text.find('\'', tempbegin) != -1: #while there are apostrophes after the beginning of the Propriety's value
+            tempbegin = text.find('\'', tempbegin)+1    #set tempbegin to apostrophe found
+            if text[tempbegin-1-1:tempbegin-1] == '\\': #if there's a backslash before the apostrophe, discard
                 continue
             else:
-                end = tempbegin-1
+                end = tempbegin-1                       #otherwise set it to the end of the propriety
                 break
         if end == 0:
             return False
         return text[begin:end]
 
-def getSettings( configFileDirectory ): #I will probably make something more automated in the future without hardcoded tag names, but with only the 6hrs experience of python I have it's gonna have to wait
-    returnObject = {
-        'torclient':{
-            'location':'', 
-            'usage':''}, 
-        'uploaderlist':[],
-        'showlist':[]
-        }
+def getProprietyList(text):
+    returnal = {}
+    proprietyLocation = 0
+    proprietyLocation = text.find(' ', proprietyLocation)     #find first propriety
+    while proprietyLocation != -1:                              #while there are spaces outside propriety value (while there are propreties)
+        beginindex = text.find('=\'', proprietyLocation)+2      #find the begin of the propriety's value
+        
+        if beginindex == -1:                    #if we had just found a random space at the end of the line or something, break
+            break
+
+        foundEnd = False
+        tempend = text.find('\'', beginindex)  #find potential end of propriety value
+        while tempend != -1:                    #while there's potential ends of propriety value
+            if text[tempend-1:tempend] == '\\': #check if before the apostrophe there's a backslash (which would indicate such apostrophe isn't the end of the propriety's value)
+                tempend = text.find('\'', tempend+1)   #find next potential end of propriety value and repeat
+                continue
+            else:                               #theres no escape backslash, this is the end of the propriety's value
+                foundEnd = True
+                break
+        if foundEnd:                    #all good
+            returnal[text[proprietyLocation+1:beginindex-2]] = text[beginindex:tempend] #set dictionary key to the value of the 
+        else:                           #it's all fucked, user is a fuckface who can't follow simple instructions on how to write a config file
+            return False
+        proprietyLocation = text.find(' ', tempend)   #find next propriety
+    return returnal                     #if no tags are found empty dictionaries evaluate to False anyway so all good
+
+
+
+def getSettings( configFileDirectory ): 
+    returnObject = {}
+
     try:
         configFile = open( configFileDirectory, 'r+')
     except IOError:
         print('could not open file')
         return False
+
+
     for line in configFile.readlines():
-        if line[:10] == '<torclient':
-            returnObject['torclient']['location'] = getPropriety(line, 'location')
-            returnObject['torclient']['usage'] = getPropriety(line, 'usage')
-        elif line[:9] == '<uploader':
-            returnObject['uploaderlist'].append(getPropriety(line, 'name'))
-        elif line[:5] == '<show':
-            returnObject['showlist'].append({
-                    'search_term':getPropriety(line, 'search_term'), 
-                    'season':getPropriety(line, 'season'), 
-                    'episode':getPropriety(line, 'episode'),
-                    'save_directory':getPropriety(line, 'save_directory')
-                    })
+        tagbegin = line.find('<')+1
+        tagend = line.find(' ', tagbegin)
+        if tagbegin == -1 or tagend == -1:  #it's either just a newline or user is fucked in the head kek
+            continue
+        tag = line[tagbegin:tagend] #get tag name
+        if tag not in returnObject: #if tag is a key in the return object, create a key with a tag's name and assign an empty list to it (to contain however many instances of that tag there may be)
+            returnObject[tag] = []
+        returnObject[tag].append(getProprietyList(line)) #push a dictionary containing the proprieties of the tag into the list
+
     configFile.close()
     return returnObject
 
-def setSettings (configFileDirectory, settings):    #same here
+
+
+def setSettings (configFileDirectory, settings):
     try:
         configFile = open( configFileDirectory, 'w')
     except IOError:
         print('could not open file')
         return False
-    configFile.write('<torclient location=\''+settings['torclient']['location']+'\' usage=\''+settings['torclient']['usage']+'\'>\n')
-    for uploader in settings['uploaderlist']:
-        configFile.write('<uploader name=\''+uploader+'\'>\n')
-    for show in settings['showlist']:
-        configFile.write ('<show search_term=\''+show['search_term']+'\' season=\''+str(show['season']).zfill(2)+'\' episode=\''+str(show['episode']).zfill(2)+'\' save_directory=\''+show['save_directory']+'\'>\n')
+
+    for key, value in settings.items():
+        if not value:
+            continue
+        for tagEntry in value:
+            if not tagEntry:
+                continue
+            configFile.write('<' + key)
+            for subkey, subvalue in tagEntry.items():
+                configFile.write(' ' + subkey + '=\'' + subvalue + '\'')
+            configFile.write('>\n')
+    configFile.close()
