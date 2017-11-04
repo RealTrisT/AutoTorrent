89aac2ccc343696159e1de4b04446e89b391ef19
TrisT <30376152+RealTrisT@users.noreply.github.com>
2017-11-04 06:56:08 +0000

    Update BitTorrentBotPython.py

--- BitTorrentBotPython.py
+++ BitTorrentBotPython.py
@@ -4,21 +4,37 @@ from config_parser import getSettings, setSettings
 import sys
 import requests
 
-fail_next_get_count = 3 #default amount of episodes its gonna search for before changing season if one is not found
+#   Return Values
+##      -2 <=> maxattempts was exceeded
+##      -1 <=> piratebay found no results for the search provided
+##       0 <=> results were found but none met the criteria
+#   Params
+##      maxAttempts -> 0 for infinite, more for a limit
+def get_ep_info(ep, uploader_list, maxAttempts = None, supressStatusCodeFailiure = None):
+    if maxAttempts is None:                 #|------------------------------------------|
+        maxAttempts = 0                     #|                                          |
+    else:                                   #|           Python Is Fucking              |  .jpg
+        maxAttempts += 1                    #|                Cancer                    |
+    if supressStatusCodeFailiure is None:   #|                                          |
+        supressStatusCodeFailiure = True    #|------------------------------------------|
 
-def get_ep_info(showname, showseason, showep, uploader_list):
-    showinfo = showname+' s'+str(showseason).zfill(2)+'e'+str(showep).zfill(2)
+
+    showinfo = ep['search_term']+' s'+str(ep['season']).zfill(2)+'e'+str(ep['episode']).zfill(2)
 
     r = requests.get('https://pirateproxy.cam/search/'+quote(showinfo, safe=''))
-    while r.status_code != 200:
-        print("Piratebay responded with status", str(r.status_code)+", retrying..")
+    while r.status_code != 200 and maxAttempts != 1:
+        if not supressStatusCodeFailiure:
+            print("Piratebay responded with status", str(r.status_code)+", retrying..")
         r = requests.get('https://pirateproxy.cam/search/'+quote(showinfo, safe=''))
+        if maxAttempts != 0:
+            maxAttempts -= 1
+    if maxAttempts == 1:
+        return -2
 
 
     if r.text.find('No hits. Try adding an asterisk in you search phrase.') != -1:
-        print('No results found for the "' + showinfo + '" entry')
-        return False
-        return
+        #print('No results found for the "' + showinfo + '" entry')
+        return -1
 
 
     variable_first = 0
@@ -28,32 +44,41 @@ def get_ep_info(showname, showseason, showep, uploader_list):
         uploaderName = ''
         magnetlink = ''
         variable_first = r.text.find('<td>\n<div class="detName">', variable_first)+1
-        if r.text[variable_first:r.text.find(' class="detLink" title', variable_first)].find('720') == -1 and r.text[variable_first:r.text.find(' class="detLink" title', variable_first)].find('1080') == -1:
+        enderoni = r.text.find(' class="detLink" title', variable_first)
+        if 'except' in ep:
+            if r.text[variable_first:enderoni].find(ep['except']) != -1:
+                continue
+        if r.text[variable_first:enderoni].find('720') == -1 and r.text[variable_first:enderoni].find('1080') == -1:
             res720or1080 = False
         else:
             res720or1080 = True
         uploaderName = r.text[r.text.find('<a class="detDesc" href="/user/', variable_first)+31:r.text.find('/" title="Browse ', variable_first)]
         magnetlink = r.text[r.text.find('</div>\n<a href="', variable_first)+16:r.text.find('" title="Download this torrent using magnet">', variable_first)]
         if (uploaderName in uploader_list) and (res720or1080 == True):
-            return {'uploader':uploaderName, 'magnetlink':magnetlink, 'search_term':showname, 'season':showseason, 'episode':showep}
+            return {'uploader':uploaderName, 'magnetlink':magnetlink, 'search_term':ep['search_term'], 'season':ep['season'], 'episode':ep['episode']}
     if is_downloading == False:
-        print("No Valid Entries Were Found For \"" + showinfo+ '"')
-        return False
+        #print("No Valid Entries Were Found For \"" + showinfo+ '"')
+        return 0
     
 def get_ep_info_checknext(show, lookAfterCount, uploader_list):
     show['episode'] = int(show['episode'])
     show['season'] = int(show['season'])
     gotShowInfo = False
     for i in range(0, lookAfterCount):
-        showinforn = get_ep_info(show['search_term'], show['season'], show['episode']+i, uploader_list)
-        if showinforn == False:
+        showinforn = get_ep_info(show, uploader_list)
+        if showinforn == 0:
+            show['episode'] += 1
             continue
+        elif showinforn == -1:
+            return False
+        elif showinforn == -2:
+            return -1
         else:
             return showinforn
     return False
 
-def download_ep(magnetlink, path, showseason, executeLocation, usage):
-    final_string = usage.replace('"%location"', executeLocation).replace('%path', path+'\\S'+str(showseason).zfill(2)).replace('%torrent', magnetlink)
+def download_ep(magnetlink, path, season, executeLocation, usage):
+    final_string = usage.replace('"%location"', executeLocation).replace('%path', path+'\\S'+str(season).zfill(2)).replace('%torrent', magnetlink)
     system(final_string)
     return True
 
@@ -73,36 +98,54 @@ def main():
     settings = getSettings(sys.argv[1].replace('"', ''))
     if settings == False:
         return 0
+
+    fail_next_get_count = 3
     if len(sys.argv) > 2:
         fail_next_get_count = int(sys.argv[2])
 
-    
-    for index, show in enumerate(settings['showlist']):
+    uploaderlist = []
+    for uploadername in settings['uploader']:
+        uploaderlist.append(uploadername['name'])
+    for index, show in enumerate(settings['show']):
+        alreadyExisting = 0
+        skippedUnmetCriteria = 0
+        downloaded = 0
+
         lastWorkingEp = show['episode'] = int(show['episode'])
         lastWorkingSe = show['season'] = int(show['season'])
         seasonSwitch = False
         while True:
-            if folderHasEp(show['save_directory']+'\\S'+str(show['season']).zfill(2), show['episode']) == True:
+            if (folderHasEp(show['save_directory']+'\\S'+str(show['season']).zfill(2), show['episode']) == True):
                 print(show['search_term']+' s'+str(show['season']).zfill(2)+'e'+str(show['episode']).zfill(2) + ' Already Present.')#log--------------
+                lastWorkingEp = show['episode']
+                lastWorkingSe = show['season']
                 show['episode'] += 1
+                seasonSwitch = False
+                alreadyExisting += 1
                 continue
-            showinfor = get_ep_info_checknext(show, fail_next_get_count, settings['uploaderlist'])
-            if showinfor == False:
+            showinfor = get_ep_info_checknext(show, fail_next_get_count, uploaderlist)
+            if showinfor == -1:
+                input('Exceeded Max Tries To Fech Page, Press Enter To Exit')
+                return
+            elif showinfor == False:
                 if seasonSwitch == True:
-                    settings['showlist'][index]['episode'] = str(lastWorkingEp).zfill(2)
-                    settings['showlist'][index]['season'] = str(lastWorkingSe).zfill(2)
-                    print('-----------Finalized Search For ' + show['search_term'] + ' With a Last Valid Tor Of: s' + str(lastWorkingSe).zfill(2)+'e'+str(lastWorkingEp).zfill(2) + '------------')#log--------------
+                    settings['show'][index]['episode'] = str(lastWorkingEp).zfill(2)
+                    settings['show'][index]['season'] = str(lastWorkingSe).zfill(2)
+                    print('-----Finalized Search For ' + show['search_term'] + ' With a Last Valid Tor Of: s' + str(lastWorkingSe).zfill(2)+'e'+str(lastWorkingEp).zfill(2) + '-----'+
+                          '\n-----> '+str(downloaded)+' downloaded, '+str(alreadyExisting) + ' already existed, ' + str(skippedUnmetCriteria) + ' skipped due to unmet criteria\n')#log--------------
                     break;
                 show['season'] += 1
                 show['episode'] = 1
                 seasonSwitch = True
                 continue
             else:
+                skippedUnmetCriteria+=showinfor['episode']-show['episode']
                 print('Fetching '+show['search_term']+' s'+str(showinfor['season']).zfill(2)+'e'+str(showinfor['episode']).zfill(2)+' from '+showinfor['uploader'])#log--------------
-                download_ep(showinfor['magnetlink'], show['save_directory'], show['season'], settings['torclient']['location'], settings['torclient']['usage'])
+                download_ep(showinfor['magnetlink'], show['save_directory'], show['season'], settings['torclient'][0]['location'], settings['torclient'][0]['usage'])
                 lastWorkingEp = showinfor['episode']
                 lastWorkingSe = showinfor['season']
                 show['episode'] = showinfor['episode']+1
+                downloaded+=1
                 seasonSwitch = False
        
     setSettings(sys.argv[1].replace('"', ''), settings)
